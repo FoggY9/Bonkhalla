7b8513473bc59571d5a6208cd0105fd396a8d72c
FoggY9 <runapriyo@gmail.com>
2022-02-23 15:05:39 +0600

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,9 +3,7 @@ module.exports = async(client, oldState, newState) => {
 
   const { Category, VcOne, VcTwo, VcThree } = require('../config.json')
   
-  
   // Static Channel Id's   
-  
   const crt_custom_lobby = VcOne;
   const crt_2s_lobby = VcTwo;
   const crt_1s_lobby = VcThree;
@@ -34,7 +32,7 @@ module.exports = async(client, oldState, newState) => {
   // Moved vc >> changed vc
   if (oldState.channel && newState.channel) {
       if (oldState.channel !== newState.channel) {
-        if (client.jointocreatemap.get(`tempvoicechannel}_${oldState.channel.id}`)) 
+        if (client.jointocreatemap.get(`tempvoicechannel_${oldState.channel.id}`)) 
         {   var vc = client.channels.cache.get(client.jointocreatemap.get(`tempvoicechannel_${oldState.channel.id}`));
                 if (vc.members.size < 1)
                  { client.jointocreatemap.delete(`tempvoicechannel_${oldState.channel.id}`); 
@@ -52,7 +50,7 @@ module.exports = async(client, oldState, newState) => {
     console.log(" :: " + user.member.user.username + "#" + user.member.user.discriminator + " :: Created " + name)
   await user.guild.channels.create(name, {  type: "GUILD_VOICE",  parent: Category,})
     .then(async vc => {
-      client.jointocreatemap.set(`tempvoicechanne_${vc.id}`, vc.id);
+      client.jointocreatemap.set(`tempvoicechannel_${vc.id}`, vc.id);
       try {await user.setChannel(vc);}
       catch (error) {setTimeout(() => {checkVc(vc)}, 1000 * 10);}
   
@@ -71,7 +69,7 @@ module.exports = async(client, oldState, newState) => {
       let channel = client.channels.cache.get(vc.id);
   if(!channel) return;
   if (vc.members.size < 1) { 
-    client.jointocreatemap.delete(`tempvoicechannel}_${vc.id}`); 
+    client.jointocreatemap.delete(`tempvoicechannel_${vc.id}`); 
     console.log(" :: " + oldState.member.user.username + "#" + oldState.member.user.discriminator + " :: Room deleted")
     return vc.delete();
   }} catch (error) {console.log(error);}
