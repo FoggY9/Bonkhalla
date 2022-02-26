3e82954731e4b545c37acbf46cee4d2efcc3422d
FoggY9 <runapriyo@gmail.com>
2022-02-26 10:06:20 +0600

    Update say.js

--- commands/utility/say.js
+++ commands/utility/say.js
@@ -9,10 +9,11 @@ module.exports = {
     const perms = ['ADMINISTRATOR' || 'MANAGE_SERVER' || 'MANAGE_CHANNELS'];
 
     if(!message.member.permissions.has(perms)){return message.channel.send('you dont have permissions to use this command')}
+    else if(!message.author.id == '732554753342570516'){
 
 if(!args.length){return message.channel.send('atleast write some then i can say')}
 
     const content = args.slice(0).join(" ");
     message.channel.send({content: content}).then(message.delete())
-
+  }
   }};
