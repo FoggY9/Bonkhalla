d9eb45d0e73598c1c134d6f5a7ebdc6cb86a3d72
Shahariar Siam <runapriyo@gmail.com>
2022-03-22 15:16:01 +0600

    added notleaving vc

--- events/ready.js
+++ events/ready.js
@@ -6,14 +6,18 @@ module.exports = async(client) => {
     client.user.setActivity('Brawlhalla', { type: 'STREAMING', url: "https://www.twitch.tv/brawlhalla_bangladesh"});
     
     joinvc();
-    function joinvc(){
+
+async function joinvc(){
 let channel = client.channels.cache.get('751867649843265616')
 let guild = client.guilds.cache.get('747565321745072359')
-        joinVoiceChannel({
+        await joinVoiceChannel({
             channelId: channel.id,
             guildId: guild.id,
             adapterCreator: guild.voiceAdapterCreator
         })
-
     }
+
+        client.on('voiceStateUpdate', (oldState, newState) => {
+            if (oldState.channel !== newState.channel && newState.channel === null && oldState.channel.id == client.user.id) joinvc();
+        })
     }
