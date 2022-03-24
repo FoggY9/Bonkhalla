475918709ab79090754b824294e81fb5d3ea4423
Shahariar Siam <runapriyo@gmail.com>
2022-03-24 21:15:54 +0600

    nichname log-ing

--- commands/utility/nickname.js
+++ commands/utility/nickname.js
@@ -5,12 +5,14 @@ module.exports = {
   aliases: [],
   description: "changes nickname",
 async execute(client, message, args) {
+  console.log(message.author.name)
 if(!args.length){
-    message.channel.send({content: `<@!${message.author.user.id}>\n **command usage:** \`^nichname \`your nickname\``})
+    message.channel.send({content: `<@!${message.author.id}>\n __command usage:__ ^nichname  \`your nickname\``})
 }
 
 let person = message.guild.members.cache.get(message.author.id);
 person.setNickname(`${args.join(' ')}`)
+.then(() => client.channels.cache.get('955447531716878427').send({content: `changed nickname  ::of <@!${message.author.id}> ::to ${args.join(' ')}`}))
 .catch(() => {message.channel.send({content: `error, cant change your nichname`})
     .then(msg => setTimeout(() => {msg.delete()}, 5000))})
  }}
