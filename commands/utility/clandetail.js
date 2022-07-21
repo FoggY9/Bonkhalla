d6bf749421971378e1cfa9a25e4ff4500cd0adac
Shahariar Siam <runapriyo@gmail.com>
2022-07-21 22:19:25 +0600

    Update clandetail.js

--- commands/utility/clandetail.js
+++ commands/utility/clandetail.js
@@ -62,7 +62,7 @@ targetRole.members.forEach(mmbr =>
 let leaderString = '';
 targetRole.members.forEach(mmbr => {
 
-if(mmbr.roles.has('851722247206600704')) leaderString = leaderString + ` <@!${mmbr.id}>`;
+if(mmbr._roles.has('851722247206600704')) leaderString = leaderString + ` <@!${mmbr.id}>`;
 })
 
   // Leaders Field
