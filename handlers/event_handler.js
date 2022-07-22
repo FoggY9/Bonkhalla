6066371e59397075a03924c2b934ecd8b37c5bba
Shahariar Siam <runapriyo@gmail.com>
2022-07-22 17:15:13 +0600

    .

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 module.exports = (client) =>{
 
-    const event_files = fs.readdirSync(`../events`).filter(file => file.endsWith('.js'));
+    const event_files = fs.readdirSync(`/events`).filter(file => file.endsWith('.js'));
 
     for(const file of event_files){
-      const events = require(`../events/${file}`);
+      const events = require(`/events/${file}`);
       const event_name = file.split('.')[0];
       client.on(event_name, events.bind(null, client));
     }
