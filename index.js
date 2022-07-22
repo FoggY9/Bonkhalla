4bcfe20699aaee6137ae0d30a8db7e47858912fe
Shahariar Siam <runapriyo@gmail.com>
2022-07-22 17:05:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('./web/index'));
+app.get('/', (req, res) => res.render('index'));
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 // Creating bot
