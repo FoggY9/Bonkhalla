af0306101687a7e93d0b3a3451632f80d46fd7a3
Shahariar Siam <runapriyo@gmail.com>
2022-08-02 16:49:28 +0600

    slash command beta
    /add

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const { joinVoiceChannel } = require('@discordjs/voice');
+const DiscordJs = require('discord.js');
 module.exports = async(client) => {
     // When Ready
     console.log('BBO bot is Online!');
@@ -29,5 +30,47 @@ function checkvcs() {
 }
 checkvcs();
 setInterval(checkvcs, 10000);
+ 
+
+
+    slashpanel();
+    function slashpanel() {
+    let guild = client.guilds.cache.get('747565321745072359');
+    let commands;
+    if (guild) {
+        commands = guild.commands
+
+    }else{
+    commands = client.application.commands
+    }
+
+    commands.create({
+        name: 'add',
+        description: 'give clan role',
+        options: [
+            {
+            name: 'clan-name',
+            description: 'the clan role you want to give',
+            choices:[
+                {name: 'Azure Spirit', value: 'Azure Spirit'}, 
+                {name: '7t1 Bangladesh', value: '7t1 Bangladesh'},
+                {name: 'Bedroom Community', value: 'Bedroom Community'},
+                {name: 'Persistence', value: 'Persistence'},
+                {name: 'Five Finger N Extra', value: 'Five Finger N Extra'},
+                {name: 'Strawberry Field', value: 'Strawberry Field'},
+                {name: 'Khudarto', value: 'Khudarto'},
+            ],
+            required: true,
+            type: DiscordJs.Constants.ApplicationCommandOptionTypes.STRING
+        },
+    {
+            name: 'target-member',
+            description: 'the member you want to give',
+            required: true,
+            type: DiscordJs.Constants.ApplicationCommandOptionTypes.USER
+    }
+    ]
+    })
+        }
  
     }
