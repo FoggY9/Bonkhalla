b4f26bf108948a0031f33d064a480a52c4124d84
Shahariar Siam <runapriyo@gmail.com>
2022-08-04 10:39:43 +0600

    fixed leaveclan performance

--- commands/utility/clandetail.js
+++ commands/utility/clandetail.js
@@ -10,58 +10,26 @@ module.exports = {
     if(!args.length){
       return message.channel.send({content: "Provide The Clan Prefix\n Usage: *^claninfo <prefix>* \n first 3 letter of a clan name is its prefix"})
     }
-
+class Clan{
+  constructor(nam,id,lo,le,li,ti) {
+      this.name = nam;
+      this.clanid = id;
+      this.logo = lo;
+      this.level = le;
+      this.link = li;
+      this.title = ti;
+  }
+}
 // Clan Info's as Objects
-let bed = {
-  name: 'Bedroom Community [SEA]',
-  clanid: '851302141499015208',
-  logo: 'https://cdn.discordapp.com/icons/869217076466835506/c4593d64d987054def3c427ad9de330a.webp?size=96',
-  level: '74',
-  link: 'https://discord.gg/Xq67vfWmzT',
-  title: `Winner of BBO Clan Battle Season One!`
-}
-let per = {
-  name: 'Persistence [SEA]',
-  clanid: '877450177991020584',
-  logo: '',
-  level: '',
-  link: `Winner of BBO Clan Battle Season two!`
-}
-let khu = {
-  name: 'Khudarto [SEA]',
-  clanid: '850939055872147456',
-  logo: '',
-  level: '',
-  link: ''
-}
-let fiv = {
-  name: 'Five Fingers n Extra [SEA]',
-  clanid: '799676323948920902',
-  logo: '',
-  level: '',
-  link: ''
-}
-let _7t1 = {
-  name: '7t1 Bangladesh [SEA]',
-  clanid: '772444761906348052',
-  logo: 'https://media.discordapp.net/attachments/903874086160191493/982970626694316032/5_METALIC.jpg',
-  level: '82',
-  link: 'https://discord.gg/PUSNgnCVjR'
-}
-let azu = {
-  name: 'Azure Spirit [SEA]',
-  clanid: '838085789841752134',
-  logo: '',
-  level: '100',
-  link: 'https://discord.gg/mFB59MeuKf'
-}
-let str = {
-  name: 'Strawberry Fields [SEA]',
-  clanid: '1000623144786198548',
-  logo: '',
-  level: '31+',
-  link: ''
-}
+let bed = new Clan('Bedroom Community [SEA]','851302141499015208','https://cdn.discordapp.com/icons/869217076466835506/c4593d64d987054def3c427ad9de330a.webp?size=96','74','https://discord.gg/Xq67vfWmzT','Winner of BBO Clan Battle Season One!');
+let per = new Clan('Persistence [SEA]','877450177991020584','','','','Winner of BBO Clan Battle Season two!');
+let khu = new Clan('Khudarto [SEA]','850939055872147456','','','','');
+let fiv = new Clan('Five Fingers n Extra [SEA]','799676323948920902','','','','');
+let _7t1 = new Clan('7t1 Bangladesh [SEA]','772444761906348052','https://media.discordapp.net/attachments/903874086160191493/982970626694316032/5_METALIC.jpg','82','https://discord.gg/PUSNgnCVjR', '');
+let azu = new Clan('Azure Spirit [SEA]','838085789841752134','lo','100','https://discord.gg/mFB59MeuKf','');
+let str = new Clan('Strawberry Fields [SEA]','1000623144786198548','','31+','','');
+let bad = new Clan(nam,id,lo,le,li,ti)
+
 
 // Created Embed
 let embed = new MessageEmbed().setColor('#FFFFFF');
@@ -113,7 +81,7 @@ if (target.link.length) {
 if (target.logo.length) {
   embed.setThumbnail(target.logo)
 }
-if (target.title) {
+if (target.title.length) {
   embed.addField(`Titles`, target.title)
 }
 
--- slashCommands/leaveclan.js
+++ slashCommands/leaveclan.js
@@ -1,18 +1,21 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: 'leaveclan',
-run: (client, interaction, options) => {
+run: async(client, interaction, options) => {
 
     interaction.reply({content: `taking action`})
     let clanrole = ['838085789841752134', '877450177991020584', '850939055872147456', '799676323948920902', '851302141499015208', '772444761906348052', '1000623144786198548', '1004325446797762611']
+
+var roles = await interaction.member.roles
+
     for(let i = 0; i < clanrole.length;i++){
-        if( interaction.member.roles.cache.has(clanrole[i])){
-          let roleName =  interaction.guild.roles.cache.find(r => r.id === clanrole[i]).name;
-            interaction.member.roles.remove(interaction.guild.roles.cache.get(clanrole[i]))
-           let done = new MessageEmbed().setColor('BLUE').setDescription(`🔵 **|** **${interaction.member.username + '#' + interaction.member.discriminator}** has removed **${roleName}** Clan Role`).setTimestamp()
+        if(roles.cache.has(clanrole[i])){
+          let roleName =  roles.cache.find(r => r.id === clanrole[i]).name;
+            roles.remove(interaction.guild.roles.cache.get(clanrole[i]))
+           let done = new MessageEmbed().setColor('BLUE').setDescription(`🔵 **|** **${interaction.member.user.username + '#' + interaction.member.user.discriminator}** has removed **${roleName}** Clan Role`).setTimestamp()
             interaction.channel.send({embeds: [done]})
         }
     }
-    interaction.channel.send({content: `${interaction.member}, clan roles are cleared`})
+    interaction.reply({content: `${interaction.member}, clan roles are cleared`, ephemeral: true})
 }
 }
