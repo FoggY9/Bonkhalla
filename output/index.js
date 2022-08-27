347a8d5d9190b82726857bf8340441a68c3bb573
Shahariar Siam <runapriyo@gmail.com>
2022-08-27 17:03:03 +0600

    Update index.js

--- output/index.js
+++ output/index.js
@@ -32,6 +32,7 @@ client.slashcmd = new discord_js_1.Collection();
     handlr.default(client);
 });
 
+keepAlive();
 const keepAlive = () => {
     server.listen(6969,() => console.log( "The server is ready aswell." ));
 }
