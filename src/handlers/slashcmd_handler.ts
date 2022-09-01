f5158e6413dc35c58a1d1bbd6fef5624f3e366be
Shahariar Siam <runapriyo@gmail.com>
2022-09-01 10:38:15 +0600

    fixing tsrun attemt 1

--- src/handlers/slashcmd_handler.ts
+++ src/handlers/slashcmd_handler.ts
@@ -1,7 +1,7 @@
 import { readdirSync } from 'fs';
 export default (client:any) =>{
 
-  const command_files = readdirSync(`./slashCommands`).filter(file => file.endsWith('.ts'));
+  const command_files = readdirSync(`./src/slashCommands`).filter(file => file.endsWith('.ts'));
 
   for(const file of command_files){
     const slashcmd = require(`../slashCommands/${file}`);
