6f622fb6a27091f2a2239539a46445250baf9be3
Shahariar Siam <runapriyo@gmail.com>
2023-04-12 21:30:49 +0600

    beta

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,37 @@
-// Necessary things
-import { Client, Collection, GatewayIntentBits } from 'discord.js';
-import dotenv from 'dotenv';
+  // Necessary things to Import
+  import { Client, Collection, GatewayIntentBits } from 'discord.js';
+  import dotenv from 'dotenv';
+  
+  dotenv.config()
 
+// login to spectator account
+const spectatorClient:any = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildPresences], allowedMentions: { parse: ['users', 'roles'], repliedUser: true }});
 
-dotenv.config()
+  // Login With Token
+spectatorClient.login(process.env['SPEC_TOKEN']);
+
+// Spectator Seek
+spectatorClient.once('ready', ()=>{
+  console.log('Spectator is Watching')
+})
+// Infinity Loop
+let check = () => {
+  let isAlive = spectatorClient.guilds.cache.get('864792830673027102').members.cache.get('732554753342570516').presence
+  if(isAlive){
+    console.log('bot is active')
+      return;
+  }else if(!isAlive){
+      runBot();
+  }
+} 
+setInterval(() => {
+  check()
+}, 20_000);
+
+// Bot launching
+function runBot() {
+
 // Express web
 const express = require('express');
 const app:any = express();
@@ -12,13 +40,10 @@ app.get('/', (req:any, res:any) => res.send('Brawlhalla bangladesh official bot 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 // Creating bot
-const client:any = new Client({ intents: [
-    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers,  GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildPresences, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], allowedMentions: { parse: ['users', 'roles'], repliedUser: true }});
+const client:any = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers,  GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], allowedMentions: { parse: ['users', 'roles'], repliedUser: true }});
 
 // Login With Token
 client.login(process.env['TOKEN']);
-
 
 // Vc Record Map 
 client.jointocreatemap = new Map();
@@ -34,3 +59,4 @@ client.slashcmd = new Collection();
   let handlr = require(`./handlers/${handler}`)
   handlr.default(client)
 })
+}
